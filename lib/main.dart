f4c5a7d84a5f2842f7c9fdcddffe70a4e38da126
rogelio-valdez-cuevas <rv114576@gmail.com>
2025-03-12 23:53:21 -0600

    Implementacion basica del reproductor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'dart:async';
+import 'package:just_audio/just_audio.dart';
+import 'package:file_picker/file_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -146,8 +148,14 @@ class HorarioPage extends StatefulWidget {
 class _HorarioPageState extends State<HorarioPage> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _selectedDay = DateTime.now();
-  Map<DateTime, List<String>> _events = {};
-  TextEditingController _eventController = TextEditingController();
+  final Map<DateTime, List<String>> _events = {};
+  final TextEditingController _eventController = TextEditingController();
+
+  @override
+  void dispose() {
+    _eventController.dispose();
+    super.dispose();
+  }
 
   void _addEvent() {
     setState(() {
@@ -205,8 +213,85 @@ class _HorarioPageState extends State<HorarioPage> {
   }
 }
 
-class ReproductorPage extends StatelessWidget {
+class ReproductorPage extends StatefulWidget {
   const ReproductorPage({super.key});
+
+  @override
+  _ReproductorPageState createState() => _ReproductorPageState();
+}
+
+class _ReproductorPageState extends State<ReproductorPage> {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  bool _isPlaying = false;
+  List<String> _songPaths = [];
+  int _currentSongIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer.playerStateStream.listen((state) {
+      setState(() {
+        _isPlaying = state.playing;
+      });
+    });
+  }
+
+  Future<void> _pickFiles() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.audio,
+      allowMultiple: true,
+    );
+
+    if (result != null) {
+      setState(() {
+        _songPaths = result.paths.whereType<String>().toList();
+      });
+    }
+  }
+
+  Future<void> _playPause() async {
+    if (_isPlaying) {
+      await _audioPlayer.pause();
+    } else {
+      if (_songPaths.isNotEmpty) {
+        await _audioPlayer.setFilePath(_songPaths[_currentSongIndex]);
+        await _audioPlayer.play();
+      }
+    }
+  }
+
+  void _playSong(int index) {
+    setState(() {
+      _currentSongIndex = index;
+    });
+    _playPause();
+  }
+
+  void _showSongList() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return ListView.builder(
+          itemCount: _songPaths.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(_songPaths[index].split('/').last),
+              onTap: () {
+                Navigator.pop(context);
+                _playSong(index);
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -214,9 +299,36 @@ class ReproductorPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Reproductor'),
         backgroundColor: Colors.blueAccent,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.folder_open),
+            onPressed: _pickFiles,
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text('Aquí irá el reproductor de música', style: TextStyle(fontSize: 18)),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Column(
+              children: [
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
+                  onPressed: _playPause,
+                ),
+                const SizedBox(height: 20),
+                if (_songPaths.isNotEmpty)
+                  Text('Reproduciendo: ${_songPaths[_currentSongIndex].split('/').last}'),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _showSongList,
+            child: const Text('Mostrar Lista de Canciones'),
+          ),
+        ],
       ),
     );
   }
